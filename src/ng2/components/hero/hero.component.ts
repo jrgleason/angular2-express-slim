52067cd66b20724214d9095e113f735935aa2e87
jrgleason <jackiegleason@gmail.com>
2016-05-22 13:11:44 -0400

    more of the tutorial

--- src/ng2/components/hero/hero.component.ts
+++ src/ng2/components/hero/hero.component.ts
@@ -7,6 +7,7 @@ import { ApplicationConstants } from '../../constants/ApplicationConstants';
     styleUrls: [ApplicationConstants.BASE_TEMPLATE_PATH + 'components/hero/hero.component.css']
 })
 export class HeroComponent {
+    heroes = HEROES;
     title = 'Tour of Heroes';
     hero: Hero = {
         id: 1,
@@ -17,3 +18,16 @@ export class Hero {
     id: number;
     name: string;
 }
+
+var HEROES: Hero[] = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+];
