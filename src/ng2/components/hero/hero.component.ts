3a6a5e256a3121060546439c63b404a5b4bac061
jrgleason <jackiegleason@gmail.com>
2016-06-12 09:51:35 -0400

    Trying to add relative pathing.

--- src/ng2/components/hero/hero.component.ts
+++ src/ng2/components/hero/hero.component.ts
@@ -12,9 +12,10 @@ import {MD_LIST_DIRECTIVES} from "@angular2-material/list/list";
 import {MD_INPUT_DIRECTIVES} from "@angular2-material/input/input";
 import {MdToolbar} from "@angular2-material/toolbar/toolbar";
 @Component({
+    module: module.id,
     selector: 'hero',
     templateUrl:'hero.component.html',
-    styleUrls: [ApplicationConstants.BASE_TEMPLATE_PATH + 'components/hero/hero.component.css'],
+    styleUrls: ['hero.component.css'],
     directives: [HeroDetailComponent, MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES, MD_LIST_DIRECTIVES, MD_ICON_DIRECTIVES, MdToolbar, MD_INPUT_DIRECTIVES],
     providers: [HeroService],
     viewProviders: [MdIconRegistry]
--- src/ng2/main.ts
+++ src/ng2/main.ts
@@ -1,4 +1,5 @@
 import {bootstrap}     from '@angular/platform-browser-dynamic';
 import {HeroComponent} from './components/hero/hero.component';
 import { HTTP_PROVIDERS } from '@angular/http';
+declare var module:any;
 bootstrap(HeroComponent, [ HTTP_PROVIDERS ]);
