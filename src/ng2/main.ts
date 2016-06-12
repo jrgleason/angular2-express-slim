93858b8ba4e7c672753ba1e99d2e3cffb938f6f0
jrgleason <jackiegleason@gmail.com>
2016-06-11 23:26:48 -0400

    Removing todo because now we do, know

--- src/ng2/main.ts
+++ src/ng2/main.ts
@@ -1,5 +1,4 @@
 import {bootstrap}     from '@angular/platform-browser-dynamic';
 import {HeroComponent} from './components/hero/hero.component';
-// TODO: Talk about why this is here and not somewhere else
 import { HTTP_PROVIDERS } from '@angular/http';
 bootstrap(HeroComponent, [ HTTP_PROVIDERS ]);
